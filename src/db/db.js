90674a53977798a442a643adf47a27c0b48597af
Federico Gabriel Gutierrez <gutierrezfedericog@gmail.com>
2024-06-24 00:18:59 -0300

    Actualización 24/06

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,8 @@
+/*
 import { createPool } from 'mysql2/promise';
 
 const pool = createPool({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
     password: '',
     database: 'peliculas',
@@ -18,5 +19,33 @@ pool.getConnection()
     .catch(err => {
         console.error('Hubo un error al conectarse a la DB:', err);
     });
+*/
+// Importa los módulos necesarios
+import { createPool } from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Configura la conexión a MySQL usando las variables de entorno
+const pool = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: parseInt(process.env.DB_PORT), // Asegúrate de convertir a número si es necesario
+    waitForConnections: true,
+    connectionLimit: 5
+});
+
+// Conectar y verificar la conexión
+pool.getConnection()
+    .then(connection => {
+        pool.releaseConnection(connection);
+        console.log('Base de datos conectada');
+    })
+    .catch(err => {
+        console.error('Hubo un error al conectarse a la DB:', err);
+    });
 
 export default pool;
+
+
